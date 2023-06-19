da01b50c07954d56f75ab3363d13753edd52fe47
ALLVIS <Alvislau2004@gmail.com>
2023-06-19 13:27:23 +0930

    finish

--- Pratical/Week 1/name.py
+++ Pratical/Week 1/name.py
@@ -5,30 +5,50 @@ print("+","-"*21,"+")
 print("|  Lua JIn Yuan ALvis   |")
 print("+","-"*21,"+")
 
-
 print(f"""
-        5*2 = _10_
-        10 - 2 = _8_
-        10.75 - 5.5 = _5.25_
-        1 + 3/4 = _1.75_
-        (3 + 5) * 2 = _16_
-        3+5*2 = _13_
-        5/2 = _2.5_
-        5 // 2 = _2_
-        7.0 / 2 = _3.5_
-        5*6*4/2 = _60.0_
-        5/2*6*4 = _60.0_
- 
-        2**2
-        2**4
-        5**2*3
-        5**(2*3)
-        1 + 3 + 5/5 + 3 + 1
-        (1 + 3 + 5)/(5 + 3 + 1) 
-        5%2
-        11 % 3 
-        9%3
+        5*2 = 10\n
+        10 - 2 = 8\n
+        10.75 - 5.5 = 5.25\n
+        1 + 3/4 = 1.75\n
+        (3 + 5) * 2 = 16\n
+        3+5*2 = 13\n
+        5/2 = 2.5\n
+        5 // 2 = 2\n
+        7.0 / 2 = 3.5\n
+        5*6*4/2 = 60.0\n
+        5/2*6*4 = 60.0\n
+        2**2 = 4\n
+        2**4 = 16\n
+        5**2*3 = 30\n
+        5**(2*3) = 15625\n
+        1 + 3 + 5/5 + 3 + 1 = 1.0\n
+        (1 + 3 + 5)/(5 + 3 + 1) = 1\n
+        5%2 = 1\n
+        11 % 3 = 2\n
+        9%3 = 0 \n
 
     """)
 
+number1 = 20
+number2 = 2
+number1 * number2
 
+#13.
+length = 5
+
+length**2
+
+#14.
+PI = 3.14
+radius = 5
+
+PI * radius**2
+
+#15.
+# First one 
+# the sentence fully print out what we want (computer can't separately ' and close')
+# second one 
+# to fix the commer problem we can use " " to let computer know the different with " " and ' '
+# third one
+# computer think the printing have been finsih when the text are scaning til to I(')m
+# "and" is one of a function in python its use to connect string when you are calling a string you just create
