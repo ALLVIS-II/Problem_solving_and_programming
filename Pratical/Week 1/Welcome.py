ae82c82e35b3882056e157aae3571db08ae133dd
ALLVIS <Alvislau2004@gmail.com>
2023-06-19 09:43:10 +0930

    adding file

---
+++ Pratical/Week 1/Welcome.py
@@ -0,0 +1,17 @@
+#
+  # File:      welcome.py
+  # Author:    Lua Jin Yuan Alvis 
+  # Email Id:  Luajy004@mymail.unisa.edu.au
+  # Version:   1.0  5 August 2014
+# # # #
+#Description: Practical 1 - displays a welcome message to the screen. This is my own work as defined by the University's
+#Academic Misconduct policy.
+
+print('\nWelcome to Problem Solving and Programming!')
+
+print(f"""
+      Author:  your name
+      Email Id:  your email id
+      This is my own work as defined by the
+      University’s Academic Misconduct Policy.
+      """)
