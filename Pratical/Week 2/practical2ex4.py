133bf8a2b5ac53219c1621ef04553f57193164f2
ALLIVS <Alvislau2004@gmail.com>
2023-06-25 21:20:55 +0930

    finsh half

---
+++ Pratical/Week 2/practical2ex4.py
@@ -0,0 +1,129 @@
+#
+# File: practical2ex4.py
+# Author: Lua Jin Yuan Alvis
+# SAIBT ID: 43018
+# Description: Practical 2, exercise 4.
+# This is my own work as defined by SAIBT's
+# Academic Misconduct policy.
+#
+
+print("a)")
+# number1 = 2
+# number2 = 20
+print(f"2")
+print(f"20")
+print(f"9\n")
+
+print("-"*50,"\n")
+
+print("b)")
+# result = 14
+print(f"14")
+# result = 21
+print(f"21\n")
+
+print("-"*50,"\n")
+
+print("c)")
+# number1 = 3
+# number1 = 5
+# number1 = 8
+print(f"8")
+
+print("-"*50,"\n")
+
+print("d)")
+# number3 = 2
+# number2 = 30 
+# number1 = 7
+print(f"7 30 2\n")
+
+print("-"*50,"\n")
+
+print("2)")
+print("Please enter your favourite colour!!")
+
+colour = input("")
+
+print(f"{colour} is a lovely colour!")
+
+team = input("")
+print(f"{team}! That's my favourite team too!\n")
+
+print("-"*50,"\n")
+
+#
+# File: practical2ex4.py
+# Author: Lua Jin Yuan Alvis
+# SAIBT ID: 43018
+# Description: Practical 2, exercise 4.
+# This is my own work as defined by SAIBT's
+# Academic Misconduct policy.
+#
+
+print("a)")
+# number1 = 2
+# number2 = 20
+print(f"2")
+print(f"20")
+print(f"9\n")
+
+print("-"*50,"\n")
+
+print("b)")
+# result = 14
+print(f"14")
+# result = 21
+print(f"21\n")
+
+print("-"*50,"\n")
+
+print("c)")
+# number1 = 3
+# number1 = 5
+# number1 = 8
+print(f"8\n")
+
+print("-"*50,"\n")
+
+print("d)")
+# number3 = 2
+# number2 = 30 
+# number1 = 7
+print(f"7 30 2\n")
+
+print("-"*50,"\n")
+
+print("2)")
+print("Please enter your favourite colour!!")
+
+colour = input("")
+
+print(f"{colour} is a lovely colour!")
+
+print("\nPlease enter your favourite football team!!")
+team = input("")
+print(f"{team}! That's my favourite team too!\n")
+
+print("-"*50,"\n")
+
+print("3)")
+
+width = 10
+height = 5
+area = width * height
+
+print(f"The area is: {area}\n")
+
+#print("Please Enter a Height and Width")
+
+finalHeight = input()
+finalWidth = input()
+
+finalArea = int(finalHeight) * int(finalWidth)
+
+print(f"The area is: {finalArea}")
+ 
+
+
+
