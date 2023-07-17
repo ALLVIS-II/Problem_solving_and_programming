78d9d889832b5dab37f39a08ad93bef87b31706f
ALLVIS <Alvislau2004@gmail.com>
2023-07-17 14:11:07 +0930

    yes

--- PSP_Assignment_1/43018.py
+++ PSP_Assignment_1/43018.py
@@ -3,8 +3,11 @@ import tkinter
 
 # The object of the TicTacToeGUI class that renders the GUI.
 # You can use this object to access the methods listed in the specification.
-ttt = tic_tac_toe_gui.TicTacToeGUI('<name as input>')
 
+
+ttt = tic_tac_toe_gui.TicTacToeGUI("")
+
+    
 # Main game loop.
 while True:
     # Add your game loop code here.
