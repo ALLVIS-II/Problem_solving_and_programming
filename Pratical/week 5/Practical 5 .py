4d232207a44524c1cf33972d8abd8e139545027b
ALLVIS <Alvislau2004@gmail.com>
2023-07-24 15:27:34 +0930

    a

---
+++ Pratical/week 5/Practical 5 .py
@@ -0,0 +1,149 @@
+print("""
+--- Question 1--- 
+What is the output produced by the following code? Check your answers using Python. 
+      \n""")
+
+# a) 3
+
+# b) 8
+
+# c) S
+# o
+# o
+# o
+#
+# m
+# u
+# c
+# h
+#
+# f
+# u
+# n
+# !
+
+# d) o
+# o
+# o
+#
+# m
+# u
+# c
+# h
+
+# f) Yes it is
+
+# g) WTF!?!
+
+
+# h) F!?!T!?!W
+
+# i) 3
+
+# j) 21 
+
+# k) 25
+
+# l) 0123456789
+
+# m) ## ## ## !
+
+print("\n--- Question 2 ---\n")
+# a)
+
+# 6
+# 8
+# 4
+# 9
+# 72
+# 9
+
+# b)
+# 2
+# four
+# 10
+# eight
+# two
+# 0
+# 2
+
+# c)
+# These
+# pretzels
+# are
+# making
+# me
+# thirsty!
+
+# d)
+# Tony Stark
+# Lex Luthor
+# Selina Kyle 
+# Bruce Wayne
+# Peter Parker
+
+# e)
+# Lex Luthor
+# Bruce Wayne
+
+print("\n--- Question 3 ---\n")
+# a)
+list1 = [7, 21, 1, 2, 5, 1, 15]
+
+# b)
+list2 = [0, 0, 0, 0, 0, 0, 0]
+
+# c)
+list2 = list1[1] + list2[3]
+
+# d)
+print(list2)
+
+print("\n--- Question 4 ---\n")
+# a) 
+xy = [4, 3, 2, 1, 0, 'new', 'old']
+
+# b)
+print(xy)
+
+# c) 
+for i in xy:
+    print(i)
+# d)
+st3 = xy[:3]
+print("st3")
+
+# e)
+st3 = xy[-3:]
+print(st3)
+
+# f)
+xy[4] = "same"
+
+# g)
+print(xy)
+
+# h)
+reversed_xy = xy[::-1]
+print(reversed_xy)
+
+print("\n--- Question 5 ---\n")
+# Use a for loop to sum the elements in the following list:
+numbers = [7, 8, 2, 0, 1, 6, 3, 4]
+
+for i in numbers:
+    ans = sum(numbers)
+    
+print(ans)
+
+# Check your answer using the Python built-in sum function.
+ans = sum(numbers)
+    
+print(ans)
+    
+    
+    
+
+
+
+
