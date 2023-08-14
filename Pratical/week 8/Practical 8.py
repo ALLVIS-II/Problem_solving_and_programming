77bb224b158294d46788cbe08a2681a10169688c
ALLVIS <Alvislau2004@gmail.com>
2023-08-14 12:37:12 +0930

    a

---
+++ Pratical/week 8/Practical 8.py
@@ -0,0 +1,30 @@
+print("----- Question 1 ----")
+
+def multiply_numbers(num1,num2,num3):
+    adds = num1*num2*num3
+    return adds
+
+a = 2
+b = 3
+c = 4
+
+result = multiply_numbers(a,b,c)
+
+print(f"the product of {a},{b},{c} are {result}")
+
+print("\n----- Question 2 -----")
+
+def maximum(a,b):
+    if a > b:
+        return a
+    else:return b
+    
+num1 = 10
+num2 = 5
+
+number = maximum(num1,num2)
+
+print(f"THe maxiume number of {num1} and {num2} is {number}")
+
+print("\n----- Question 3 -----")
+
